f066fd51e7b98db16d0b87a3f959dec7af72e82e
Jitgitbit <rowowolf@gmail.com>
2020-04-14 11:02:50 +0200

    basic variables ready

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {         // EVERYTHING has to be in here !
   const squares = document.querySelectorAll('.grid div')
+  const result = document.querySelector('#result')
+  const displayCurrentPlayer = document.querySelector('#current-player')
+
+  let currenPlayer = 1
 })
